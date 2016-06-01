508a71b3e14c6e0ffda093845f04373bb4794820
ivanybma <ivanybma@yahoo.com>
2016-06-01 17:26:46 +0800

    by b

--- src/main/java/com/springapp/Dao/Dao.java
+++ src/main/java/com/springapp/Dao/Dao.java
@@ -13,5 +13,5 @@ public interface Dao<T extends Object> {
     public void deleteById(Serializable id);
     public void update(T st);
     public List<T> query(String id);
-
+    System.out.println("test by b");
 }
