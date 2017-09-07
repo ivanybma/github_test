00a42cea64099463e50b59f8fa61a007129add85
ivanybma <ivanybma@yahoo.com>
2017-09-06 21:45:57 -0400

    branch1 changes

--- src/main/java/com/springapp/Dao/Dao.java
+++ src/main/java/com/springapp/Dao/Dao.java
@@ -13,6 +13,7 @@ public interface Dao<T extends Object> {
     public void deleteById(Serializable id);
     public void update(T st);
     public List<T> query(String id);
+    System.out.println("branch1");
     System.out.println("by B”);
 //    System.out.println("by A");
 //    System.out.println("test by b");
